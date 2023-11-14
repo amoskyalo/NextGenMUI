fa31d57730d84e401ab86b42e61a40c3a3fb037c
Amos Kyalo <amoskyalo927@gmail.com>
2023-11-14 19:05:02 +0300

    Fixed heigh issues

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
-import { useState } from "react";
-import PopoverModel from "./lib/components/Popovers/PopoverModel";
-import StepperFormModel from "./lib/components/Forms/StepperFormModel";
 import FormModel from "./lib/components/Forms/FormModel";
 
 function App() {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-
-
-  const handleOpenPopover = (event) => {
-    setAnchorEl(event.currentTarget);
-  }
-
   const inputs = [
     {
       name: "userName",
       type: "text",
       label: "Full Name",
       value: null,
-      required: true
     },
     {
       name: "userPhone",
       label: "Phone Number",
-      value: null,
-      required: true,
-      type: "tel"
+      type: "tel",
+      value: null
     },
     {
       name: "userEmail",
       label: "Email Address",
-      value: null,
-      required: true,
-      type: "email"
+      type: "email",
+      value: null
     }
   ]
 
   return (
     <>
       <div style={{ width: 300, margin: "auto", marginTop: 200 }}>
-        <FormModel inputs={inputs}
+        <FormModel
+          inputs={inputs}
           CustomTitle={() => <p style={{ fontSize: 18, fontWeight: 900 }}>Add New User</p>}
           onSubmit={(values) => { console.log(values) }}
           options={{
