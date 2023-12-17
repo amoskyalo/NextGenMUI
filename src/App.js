ab7388ed09c5ecb5c4da959d9338dc69b2c562bb
Amos Kyalo <amoskyalo927@gmail.com>
2023-12-17 10:11:29 +0300

    Fixed few issues on stepperFormModel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,64 @@
+import { Button } from "@mui/material";
+import StepperFormModel from "./lib/components/Forms/StepperFormModel";
+
 function App() {
+  const steps = [
+    {
+      label: "Step 1",
+      inputs: [
+        {
+          name: "username",
+          label: "Username",
+          type: "text",
+          value: "",
+        },
+        {
+          name: "password",
+          label: "Password",
+          type: "password",
+          value: "",
+        },
+      ],
+    },
+    {
+      label: "Step 2",
+      inputs: [
+        {
+          name: "isAdmin",
+          label: "Is Admin",
+          lookups: [
+            { title: "Yes", value: 0 },
+            { title: "No", value: 1 },
+          ],
+          value: "",
+        },
+        {
+          name: "roles",
+          label: "Roles",
+          lookups: [
+            { title: "Edit", value: 3 },
+            { title: "Delete", value: 6 },
+            { title: "Add", value: 12 },
+          ],
+          multiple: true,
+          value: [{ title: "Edit", value: 3 }], //will be passed as the default values for this input,
+        },
+      ],
+    },
+  ];
+
+  const CustomSubmitButton = () => (
+    <Button variant="contained">Add To Table</Button>
+  );
+
   return (
-    <p>Welcome to next gen MUI. Navigate to lib folder to start building your component</p>
+    <StepperFormModel
+      steps={steps || []}
+      width={400}
+      onSubmit={(values) => console.log(values)}
+      CustomSubmitButton={CustomSubmitButton}
+    //other props
+    />
   );
 }
 
