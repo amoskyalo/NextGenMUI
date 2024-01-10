b5f8fca604bdb9f5eab169cc5b721782b5c8c9fc
Amos Kyalo <amoskyalo927@gmail.com>
2024-01-10 22:14:51 +0300

    Added dates field and fixed boolean

--- dist/components/Forms/InputTypes/Boolean.js
+++ dist/components/Forms/InputTypes/Boolean.js
@@ -1,65 +1,93 @@
 "use strict";
 
-require("core-js/modules/es.object.assign.js");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
 require("core-js/modules/web.dom-collections.iterator.js");
+require("core-js/modules/es.array.includes.js");
+require("core-js/modules/es.string.includes.js");
 var _react = _interopRequireDefault(require("react"));
 var _material = require("@mui/material");
-const _excluded = ["value", "label"];
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-const renderBoolean = _ref => {
+const CheckboxOption = _ref => {
+  let {
+    name,
+    value,
+    label,
+    isChecked,
+    onChange,
+    checkboxColor,
+    labelColor
+  } = _ref;
+  return /*#__PURE__*/_react.default.createElement(_material.Box, {
+    key: value,
+    sx: {
+      display: "flex",
+      alignItems: "center",
+      columnGap: 1
+    }
+  }, /*#__PURE__*/_react.default.createElement(_material.Checkbox, {
+    sx: {
+      m: 0,
+      px: 0,
+      color: checkboxColor
+    },
+    name: name,
+    checked: isChecked,
+    onChange: onChange
+  }), /*#__PURE__*/_react.default.createElement(_material.Typography, {
+    sx: {
+      opacity: "70%",
+      color: labelColor
+    }
+  }, label));
+};
+const renderBoolean = _ref2 => {
   let {
     booleanOptions,
     name,
-    onChange: _onChange,
-    formik
-  } = _ref;
+    onChange,
+    formik,
+    multiple
+  } = _ref2;
+  const isError = formik.touched[name] && Boolean(formik.errors[name]);
+  const checkboxColor = isError ? "#d33247" : undefined;
+  const labelColor = isError ? "#d33247" : undefined;
+  const isChecked = value => multiple ? Array.isArray(formik.values[name]) && formik.values[name].includes(value) : formik.values[name] === value;
+  const handleCheckboxChange = (value, event) => {
+    if (multiple) {
+      const updatedValues = event.target.checked ? [...formik.values[name], value] : formik.values[name].filter(item => item !== value);
+      formik.setFieldValue(name, updatedValues);
+    } else {
+      formik.setFieldValue(name, event.target.checked ? value : null);
+    }
+    if (typeof onChange === 'function') onChange(event);
+  };
   return /*#__PURE__*/_react.default.createElement(_material.Box, {
     sx: {
       display: "flex",
       columnGap: 4,
       flexWrap: "wrap"
     }
-  }, booleanOptions.map(_ref2 => {
-    let {
-        value,
-        label
-      } = _ref2,
-      otherProps = _objectWithoutProperties(_ref2, _excluded);
-    return /*#__PURE__*/_react.default.createElement(_material.Box, {
+  }, booleanOptions.map(option => {
+    const {
+      value,
+      label
+    } = typeof option === "object" ? option : {
+      value: option,
+      label: option
+    };
+    return /*#__PURE__*/_react.default.createElement(CheckboxOption, {
       key: value,
-      sx: {
-        display: "flex",
-        alignItems: "center",
-        columnGap: 1
-      }
-    }, /*#__PURE__*/_react.default.createElement(_material.Checkbox, _extends({
-      sx: {
-        m: 0,
-        px: 0,
-        color: Boolean(formik.errors[name]) ? "#d33247" : null
-      },
       name: name,
-      checked: formik.values[name] === value,
-      onChange: event => {
-        const newValue = event.target.checked ? value : null;
-        formik.setFieldValue(name, newValue, true);
-        if (_onChange) {
-          _onChange(event);
-        }
-      }
-    }, otherProps)), /*#__PURE__*/_react.default.createElement(_material.Typography, {
-      sx: {
-        opacity: "70%",
-        color: Boolean(formik.errors[name]) ? "#d33247" : null
-      }
-    }, label));
+      value: value,
+      label: label,
+      isChecked: isChecked(value),
+      onChange: event => handleCheckboxChange(value, event),
+      checkboxColor: checkboxColor,
+      labelColor: labelColor
+    });
   }));
 };
 const BooleanField = _ref3 => {
@@ -70,6 +98,7 @@ const BooleanField = _ref3 => {
       label,
       name,
       onChange,
+      multiple,
       renderField
     }
   } = _ref3;
@@ -81,18 +110,20 @@ const BooleanField = _ref3 => {
       onChange
     }
   };
-  const content = typeof renderField === "function" && /*#__PURE__*/_react.default.isValidElement(renderField(inputProps)) ? renderField(inputProps) : /*#__PURE__*/_react.default.createElement(_material.FormControl, null, /*#__PURE__*/_react.default.createElement(_material.Typography, {
+  const isError = formik.touched[name] && Boolean(formik.errors[name]);
+  const labelColor = isError ? "#d33247" : null;
+  const content = typeof renderField === 'function' && /*#__PURE__*/_react.default.isValidElement(renderField(inputProps)) ? renderField(inputProps) : /*#__PURE__*/_react.default.createElement(_material.FormControl, null, /*#__PURE__*/_react.default.createElement(_material.Typography, {
     sx: {
       opacity: "70%",
-      color: Boolean(formik.errors[name]) ? "#d33247" : null
+      color: labelColor
     }
   }, label), renderBoolean({
     formik,
     booleanOptions,
-    label,
     name,
-    onChange
-  }), formik.touched[name] && formik.errors[name] && /*#__PURE__*/_react.default.createElement(_material.Typography, {
+    onChange,
+    multiple
+  }), isError && /*#__PURE__*/_react.default.createElement(_material.Typography, {
     sx: {
       fontSize: 12,
       color: "#d33247",
